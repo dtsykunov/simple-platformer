b190c03723022d1db97b926c97fe8d1c326b363a
dtsykunov <73191463+dtsykunov@users.noreply.github.com>
2025-08-25 23:39:51 +0300

    add multipliers

--- autoloads/resource_manager.gd
+++ autoloads/resource_manager.gd
@@ -11,19 +11,27 @@ enum ResourceType {
 	GOAL,
 }
 
-@export var default_resources := {
+@export var default_resources: Dictionary[ResourceType, int] = {
 	ResourceType.MONEY: 0, # you can leave those blank this is just what you start with
 	ResourceType.OXYGEN: MAX_OXYGEN,
 	ResourceType.GOAL: 1,
 }
 
+@export var default_multipliers: Dictionary[ResourceType, int] = {
+	ResourceType.MONEY: 1,
+	ResourceType.OXYGEN: 1,
+}
+
 var resources := {}
-var resource_labels := {}
+var multipliers := {}
 
 func _ready() -> void:
-	reset_resources()
+	reset()
 
-func add_resource(resource_name: ResourceType, value: int):
+func add_resource(resource_name: ResourceType, value: int, ignore_multipliers: bool = false) -> void:
+	if not ignore_multipliers:
+		value = _apply_multipliers(resource_name, value)
+
 	if not resources.has(resource_name):
 		set_resource_value(resource_name, value)
 		return
@@ -41,5 +49,25 @@ func set_resource_value(resource_name: ResourceType, value: int):
 	resources[resource_name] = value
 	resources_changed.emit()
 
-func reset_resources() -> void:
+func try_complete_goal() -> void:
+	var goal = resources[ResourceType.GOAL]
+	if resources[ResourceType.MONEY] >= goal:
+		add_resource(ResourceType.GOAL, +1)
+		add_resource(ResourceType.MONEY, -goal, true) # ignore multipliers when subtracting money
+		set_resource_value(ResourceType.OXYGEN, _apply_multipliers(ResourceType.OXYGEN, MAX_OXYGEN))
+		Global.level_objective_reached.emit()
+
+func _apply_multipliers(resource_name: ResourceType, value: int) -> int:
+	if not multipliers.has(resource_name):
+		return value
+	return value * multipliers[resource_name]
+
+func reset() -> void:
+	_reset_resources()
+	_reset_multipliers()
+
+func _reset_resources() -> void:
 	resources = default_resources.duplicate()
+
+func _reset_multipliers() -> void:
+	multipliers = default_multipliers.duplicate()
--- main/main.gd
+++ main/main.gd
@@ -40,5 +40,5 @@ func finish_game() -> void:
 
 func _unhandled_key_input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_cancel"):
-		ResourceManager.reset_resources()
+		ResourceManager.reset()
 		get_tree().reload_current_scene()
