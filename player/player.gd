0b2335052423840893ad3a0b7e88297f9a49a290
dtsykunov <73191463+dtsykunov@users.noreply.github.com>
2025-08-27 21:00:59 +0300

    fix animation 2

--- player/player.gd
+++ player/player.gd
@@ -5,6 +5,12 @@ extends CharacterBody2D
 
 @onready var last_player_pos = position
 
+enum State {
+	IDLE,
+	WALKING,
+}
+var state: State = State.IDLE
+
 var inputs = {
 	"right": Vector2.RIGHT,
 	"left": Vector2.LEFT,
@@ -23,16 +29,22 @@ func _physics_process(_delta: float) -> void:
 			move(dir)
 
 func move(dir):
+	if state != State.IDLE:
+		return
 	anim.rotation = inputs[dir].angle()
 
 	ray.target_position = inputs[dir] * Global.tile_size
 	ray.force_raycast_update()
 	if !ray.is_colliding():
+		state = State.WALKING
 		var new_position = position + inputs[dir] * Global.tile_size
 		var tween = create_tween()
-		tween.tween_property(self, "position", new_position, 0.3).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT)
+		tween.tween_property(self, "position", new_position, 0.1).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT)
+		tween.finished.connect(func():
+			state = State.IDLE
+			anim.play("idle")
+		)
 		anim.play("walk")
-
 		if last_player_pos.y != new_position.y and new_position.y < 3 * Global.tile_size: # 3 for space tiles maybe add a check var
 			Global.player_reached_surface.emit()
 		ResourceManager.add_resource(ResourceManager.ResourceType.OXYGEN, -1)
@@ -41,5 +53,4 @@ func move(dir):
 
 	Global.grid_spawner.use_cell_at_position(ray.target_position + position)
 	anim.play("mine")
-
 	ResourceManager.add_resource(ResourceManager.ResourceType.OXYGEN, -1)
