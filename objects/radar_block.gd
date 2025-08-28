1dbc020d3b36b5f58b70b4240aebb3a7ef168068
dtsykunov <73191463+dtsykunov@users.noreply.github.com>
2025-08-28 20:17:07 +0300

    remove unnecessary code

--- objects/radar_block.gd
+++ objects/radar_block.gd
@@ -3,17 +3,6 @@ extends Node2D
 @export var block_density: int = 2
 @export var reveal_radius: int = 2
 
-@onready var tile_map_layer: TileMapLayer = $"../../MineTileMapLayer"
-@onready var tile_map_layer2: TileMapLayer = $"../../FogTileMapLayer"
-@onready var objects_manager: Node2D = $"../../../ObjectsManager"
-
-var grid_pos: Vector2i
-
-
-func _ready() -> void:
-	grid_pos = tile_map_layer.local_to_map(global_position)
-
-
 func use_object() -> void:
 	block_density -= 1
 	if block_density <= 0:
