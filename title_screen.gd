2f70a706263f3f6f5c80d2e6ad3bd02bfb85f84f
TobiasDemuth <150724275+TobiasDemuth@users.noreply.github.com>
2025-08-30 19:30:28 +0000

    title screen at start

--- title_screen.gd
+++ title_screen.gd
@@ -7,10 +7,12 @@ extends Control
 @export var particle_scene : PackedScene
 
 var allow = false
+var started = false
 
 func _ready() -> void:
 	_emit_particle()
 	video.finished.connect(_on_video_finished)
+	video.play()
 
 func _unhandled_input(event: InputEvent) -> void:
 	if !allow:
@@ -29,6 +31,9 @@ func _on_video_finished() -> void:
 	allow = true
 
 func _start_main() -> void:
+	if started:
+		return
+	started = true
 	animation.play("zoom")
 	await animation.animation_finished
 	get_tree().change_scene_to_file("res://main/main.tscn")
