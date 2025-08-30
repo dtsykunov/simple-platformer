d5e2a5c9b6c0ba336ead1e033e9a08ee521104f2
TobiasDemuth <150724275+TobiasDemuth@users.noreply.github.com>
2025-08-30 14:37:12 +0000

    title screen video

--- title_screen.gd
+++ title_screen.gd
@@ -1,16 +1,29 @@
 extends Control
 
-@export var animation : AnimationPlayer
+@export var animation: AnimationPlayer
+@export var video: VideoStreamPlayer
+@export var layer: CanvasLayer
+
+var allow = false
+
+func _ready() -> void:
+	video.finished.connect(_on_video_finished)
 
 func _unhandled_input(event: InputEvent) -> void:
+	if !allow:
+		return
 	if event is InputEventKey and event.pressed:
 		_start_main()
 
 func _gui_input(event: InputEvent) -> void:
-
+	if !allow:
+		return
 	if event.is_pressed():
 		_start_main()
 
+func _on_video_finished() -> void:
+	layer.queue_free()
+	allow = true
 
 func _start_main() -> void:
 	animation.play("zoom")
